a5c1f0e22b0e28c292385718a82283d31c5e0077
user <mvf@usp.br>
2018-10-08 17:13:50 -0300

    one more library to load; rewrote tab names loading; rewrote dataset loading; tested if var names match; rbinded them all in one long dataset; saved them to disk

--- 1_importa.R
+++ 1_importa.R
@@ -1,24 +1,45 @@
 # script de importação de dados
 
+# carregando os pacotes que serão utilizados
+library(magrittr)
 library(tidyverse)
 
-# uma aba para cada espécie -- exceto a última aba, que contém metadados
-abas <-
-  readxl::excel_sheets("dados/Data_species.xlsx") %>%
-  head(-1)
+# uma aba para cada espécie
+abas <- readxl::excel_sheets("dados/Data_species.xlsx")
 
-# fase de teste: analisar só os dados de Leopardus colocolo,
-#   pois faltam as coordenadas nas outras abas
-dados <- readxl::read_xlsx("dados/Data_species.xlsx", sheet = "Leopardus_colocolo")
+# carregando os conjuntos de dados de cada espécie (um por aba)
+dados <-
+  sapply(
+    abas,
+    function(aba)
+      readxl::read_xlsx("dados/Data_species.xlsx", sheet = aba),
+    simplify = F,
+    USE.NAMES = T
+  )
 
-# # quando tiver em mãos a versão do arquivo .xlsx com as coordenadas em todas as abas
-# dados <- list()
-# for (aba in abas) {
-#   dados[[aba]] <-
-#     readxl::read_xlsx("dados/Data_species.xlsx", sheet = aba)
-#    etc
-# }
+# as variáveis estão com os mesmos nomes em todas as abas?
+dados %>%
+  lapply(names) %>%
+  as.data.frame(stringsAsFactors = F) %>%
+  t() %>%
+  as.data.frame() %>%
+  summarise_all(nlevels)
+# só a presença/ausência por espécie (17a) é que não
+# está com o nome de cada espécie em cada planilha
 
+# juntando todas os conjuntos de dados em um conjunto de dados único
+todos <-
+  lapply(
+    abas,
+    function(aba)
+      dados[[aba]] %>%
+      rename_at(ends_with("_P_A"), "Presence") %>% ## tá dando pau aqui
+      mutate(Species = aba)
+  ) %>%
+  bind_rows() %>%
+  rename(Latitude = latitude, Longitude = longitude) # trocar esse código depois
 
-
+# salvando os objetos criados
+saveRDS(dados, "dados.RDS")
+saveRDS(todos, "todos.RDS")
 
